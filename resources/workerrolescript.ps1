04d26bee80c43637368da3f7044f1e195b0162e9
Norton Pengra <npengra317@gmail.com>
2016-09-23 11:25:47 -0700

    update worker role script

--- resources/workerrolescript.ps1
+++ resources/workerrolescript.ps1
@@ -6,63 +6,18 @@ if (Test-Path "E:\approot\") {
     throw "approot not found"
 }
 
-$scheduled_task = "<?xml version=`"1.0`" encoding=`"UTF-16`"?>
-<Task version=`"1.4`" xmlns=`"http://schemas.microsoft.com/windows/2004/02/mit/task`">
-  <RegistrationInfo>
-    <Date>2016-09-06T20:27:10.2939543</Date>
-    <Author>Norton Pengra</Author>
-  </RegistrationInfo>
-  <Triggers>
-    <BootTrigger>
-      <Enabled>true</Enabled>
-    </BootTrigger>
-  </Triggers>
-  <Principals>
-    <Principal id=`"Author`">
-      <UserId>S-1-5-18</UserId>
-      <RunLevel>LeastPrivilege</RunLevel>
-    </Principal>
-  </Principals>
-  <Settings>
-    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
-    <DisallowStartIfOnBatteries>true</DisallowStartIfOnBatteries>
-    <StopIfGoingOnBatteries>true</StopIfGoingOnBatteries>
-    <AllowHardTerminate>true</AllowHardTerminate>
-    <StartWhenAvailable>false</StartWhenAvailable>
-    <RunOnlyIfNetworkAvailable>false</RunOnlyIfNetworkAvailable>
-    <IdleSettings>
-      <StopOnIdleEnd>true</StopOnIdleEnd>
-      <RestartOnIdle>false</RestartOnIdle>
-    </IdleSettings>
-    <AllowStartOnDemand>true</AllowStartOnDemand>
-    <Enabled>true</Enabled>
-    <Hidden>false</Hidden>
-    <RunOnlyIfIdle>false</RunOnlyIfIdle>
-    <DisallowStartOnRemoteAppSession>false</DisallowStartOnRemoteAppSession>
-    <UseUnifiedSchedulingEngine>false</UseUnifiedSchedulingEngine>
-    <WakeToRun>false</WakeToRun>
-    <ExecutionTimeLimit>PT0S</ExecutionTimeLimit>
-    <Priority>7</Priority>
-    <RestartOnFailure>
-      <Interval>PT1M</Interval>
-      <Count>3</Count>
-    </RestartOnFailure>
-  </Settings>
-  <Actions Context=`"Author`">
-    <Exec>
-      <Command>C:\python\python.exe</Command>
-      <Arguments>$approot\worker.py</Arguments>
-    </Exec>
-  </Actions>
-</Task>"
+Invoke-WebRequest "https://raw.githubusercontent.com/qwergram/Django-Azure-PaaS-Guide/master/resources/schtask_workrole.xml" -OutFile "$approot\schedule.xml"
+(Get-Content "$approot\schedule.xml") | Foreach-Object {$_ -replace "{{approot}}", $approot} | Out-File "$approot\schedule.xml" -Encoding ascii
 
-try {
+try { 
+    # First Test if python is already installed
     Start-Process -FilePath "python" -ArgumentList "-c `"print('hello world')`"" -ErrorAction Stop -Wait
 } catch {
+    # Download it if it doesn't
     Invoke-WebRequest "https://www.python.org/ftp/python/3.5.2/python-3.5.2.exe" -OutFile "$approot\install_python.exe"
     Start-Process -FilePath "$approot\install_python.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 DefaultAllUsersTargetDir=`"C:\\python\\`"" -Wait
-    Start-Process -FilePath "C:\python\Scripts\pip.exe" -ArgumentList "install -r $approot\requirements.txt" -Wait
 }
+Start-Process -FilePath "C:\python\Scripts\pip.exe" -ArgumentList "install -r $approot\requirements.txt" -Wait
 
 if (Test-Path "$env:ProgramFiles\microsoft\web platform installer\WebpiCmd-x64.exe") {
     Start-Process -FilePath "$env:ProgramFiles\microsoft\web platform installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:ARR /accepteula" -Wait
@@ -74,8 +29,7 @@ if (Test-Path "$env:ProgramFiles\microsoft\web platform installer\WebpiCmd-x64.e
     Start-Process -FilePath "$env:ProgramFiles\microsoft\web platform installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:ARR /accepteula" -Wait
 }
 
-$scheduled_task | Out-File "$approot\schedule.xml" -Encoding ascii
-Start-Process -FilePath "schtasks" -ArgumentList "-Delete -TN `"WebServer`" /F" -Wait
+Start-Process -FilePath "schtasks" -ArgumentList "-Delete -TN `"WorkerRole`" /F" -Wait
 Start-Process -FilePath "taskkill" -ArgumentList "/IM python.exe /F" -Wait
-Start-Process -FilePath "schtasks" -ArgumentList "-Create -XML `"$approot\schedule.xml`" -TN `"WebServer`"" -Wait
-Start-Process -FilePath "schtasks" -ArgumentList "-Run -TN `"WebServer`"" -Wait
+Start-Process -FilePath "schtasks" -ArgumentList "-Create -XML `"$approot\schedule.xml`" -TN `"WorkerRole`"" -Wait
+Start-Process -FilePath "schtasks" -ArgumentList "-Run -TN `"WorkerRole`"" -Wait
